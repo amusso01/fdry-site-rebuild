cb323bd115fc8cb4d888b00080d5e43fa0b30abc
Andrea Musso <eandram@hotmail.it>
2026-09-10 12:41:27 +0200

    Implement new service template and update homepage template
    - Added `template-service.php` for a new service page layout, including centered content and a new header.
    - Updated `template-home.php` to uncomment and include previously commented template parts for improved homepage structure.
    - Introduced ACF configuration for the new service template in `acf-json/group_6aa185527a283.json`.
    - Updated `function-dev.php` to register the new service template for use in the WordPress admin.

--- library/function-dev.php
+++ library/function-dev.php
@@ -49,6 +49,7 @@ function fdry_acf_only_page_templates(): array
 {
 	return array(
 		'template-home.php',
+		'template-service.php',
 	);
 }
 
---
+++ template-service.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Template Name: NEW template Service
+ * 
+ * Template Post Type: page
+ *
+ * 
+ *
+ */
+
+if (! defined('ABSPATH')) {
+  exit; // Exit if accessed directly.
+}
+get_header('new');
+?>
+
+
+<main class="main service-main" role="main">
+
+</main>
+
+<?php get_footer(); ?>
